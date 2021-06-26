02f30c63fbaf8db76833d218918bbf99c79c5709
ADITYA <a.aditya041422@gmail.com>
2021-06-26 18:30:42 +0530

    files added

---
+++ FireDB/app/src/main/java/com/example/firedb/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.firedb;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.example.firedb.databinding.ActivityMainBinding;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+
+import java.util.UUID;
+
+public class MainActivity extends AppCompatActivity {
+    DatabaseReference databaseReference;
+    StorageReference storageReference;
+    ActivityMainBinding binding;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        /*setContentView(R.layout.activity_main);*/
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main);
+        databaseReference = FirebaseDatabase.getInstance().getReference("UserData");
+        storageReference = FirebaseStorage.getInstance().getReference()
+                .child("Images/"+ UUID.randomUUID().toString());
+
+        binding.iv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent();
+                i.setType("image/");
+                //for pdf : i.setType("application/pdf")
+                i.setAction(Intent.ACTION_GET_CONTENT);
+                /*This is used to select an image from your device*/
+                startActivityForResult(Intent.createChooser(i,"SelectImage"),0);
+            }
+        });
+    }
+
+    public void save(View view) {
+    }
+
+    public void fetch(View view) {
+    }
+}
