8a28be0413374c6276f33ec27169e49611e0a131
ADITYA <a.aditya041422@gmail.com>
2021-07-03 18:19:06 +0530

    files added

--- DeviceLocationDetails/app/src/main/java/com/example/devicelocationdetails/MainActivity.java
+++ DeviceLocationDetails/app/src/main/java/com/example/devicelocationdetails/MainActivity.java
@@ -3,12 +3,17 @@ package com.example.devicelocationdetails;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
+import androidx.fragment.app.FragmentActivity;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.location.Address;
 import android.location.Geocoder;
 import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
@@ -16,6 +21,14 @@ import android.widget.TextView;
 import com.example.devicelocationdetails.R;
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptor;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 
@@ -23,9 +36,13 @@ import java.io.IOException;
 import java.util.List;
 import java.util.Locale;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends FragmentActivity implements OnMapReadyCallback {
     TextView tv;
     FusedLocationProviderClient locationProviderClient;
+    GoogleMap map;
+    LocationManager locationManager;
+    SupportMapFragment fragment;
+    double lat, lon;
 
     @Override
 
@@ -34,6 +51,30 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         tv = findViewById(R.id.textView);
         locationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        LocationListener listener = new LocationListener() {
+            @Override
+            public void onLocationChanged(@NonNull Location location) {
+                lat = location.getLatitude();
+                lon = location.getLongitude();
+                fragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+                assert fragment != null;
+                fragment.getMapAsync(MainActivity.this);
+                locationProviderClient = LocationServices.getFusedLocationProviderClient(MainActivity.this);
+            }
+        };
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1, 1, listener);
+        locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER,1,1,listener);
     }
 
     public void getDeviceLocationDetails(View view) {
@@ -70,5 +111,17 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        map = googleMap;
+        BitmapDescriptor descriptor = BitmapDescriptorFactory.fromResource(R.drawable.ic_baseline_my_location_24);
+        //Bitmap bitmap = BitmapFactory.decodeResource(getResources(),)
+        LatLng latLng = new LatLng(lat,lon);
+        map.clear();
+        map.addMarker(new MarkerOptions().position(latLng).title("My Home").icon(descriptor));
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng,15.0f));
+        map.getUiSettings().setZoomControlsEnabled(true);
     }
 }
